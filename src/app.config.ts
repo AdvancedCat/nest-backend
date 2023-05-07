0041d9acc0ac7436195abcf38be016631bd93e74
advancedcat <wshx1938@163.com>
2023-05-07 15:30:06 +0800

    let app running.

---
+++ src/app.config.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+
+
+const ROOT_PATH = path.join(__dirname, '..');
+const PROJECT_ROOT = path.join(__dirname, '..');
+
+export const APP = {
+  ROOT_PATH,
+  PORT: 3000,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { APP } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(APP.PORT);
 }
+
 bootstrap();
