a26d3d7dc02a0fdf833dd423018205f9f62021ff
advancedcat <wshx1938@163.com>
2023-05-11 16:45:45 +0800

    add article create dto

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { APP } from './app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe())
+
+  // config for swagger
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('Median API description')
