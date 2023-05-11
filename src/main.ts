52d5894f1ea7acf68a2d15aaf7ab2c8cb47fd846
advancedcat <wshx1938@163.com>
2023-05-11 16:49:41 +0800

    whitelist in ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { APP } from './app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })) // whitelist: true - remove all properties that are not in the DTO
 
   // config for swagger
   const config = new DocumentBuilder()
