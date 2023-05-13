48b8f5cb0fb6a007a730a8b3cdff42bdd70889af
advancedcat <wshx1938@163.com>
2023-05-13 15:18:48 +0800

    auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 
 // Database
-import {PrismaModule} from './modules/prisma/prisma.module'
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 // Biz
 import { ArticlesModule } from './modules/articles/articles.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 // Services
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, UsersModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
